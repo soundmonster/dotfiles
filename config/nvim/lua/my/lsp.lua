0231f4d503eabdd56baa3f8c9cf6c4ada278bfeb
Leo B <leo.the.soundmonster@gmail.com>
2023-09-05 17:08:23 +0200

    Update nerdfont icons

--- config/nvim/lua/my/lsp.lua
+++ config/nvim/lua/my/lsp.lua
@@ -8,7 +8,8 @@ local lspkind = require("lspkind")
 local luasnip = require("luasnip")
 
 -- setup diagnostic symbols for sign column
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+-- TODO these signs are off, needs an update
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
