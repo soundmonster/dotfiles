5ac11a08fca73cc34841e3857d41336fc4929943
Leo B <404293+soundmonster@users.noreply.github.com>
2026-01-27 09:59:53 +0100

    wip

--- dot_config/nvim/lua/my/lsp.lua
+++ dot_config/nvim/lua/my/lsp.lua
@@ -274,7 +274,7 @@ null_ls.setup({
     -- null_ls.builtins.diagnostics.credo,
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.completion.luasnip,
-    null_ls.builtins.diagnostics.yamllint,
+    -- null_ls.builtins.diagnostics.yamllint,
     null_ls.builtins.diagnostics.zsh,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.shellharden,
